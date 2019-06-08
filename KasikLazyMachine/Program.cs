7045d6c3502a16556514f7c6eeff0c9c5e24538b
Ricardo Figueroa Peregrina <dragoone_the_wolf@hotmail.com>
2019-06-08 08:45:01 -0500

    Included MD5 check to reduce traffic to Delta's website and added support for build templates.

--- KasikLazyMachine/Program.cs
+++ KasikLazyMachine/Program.cs
@@ -1,26 +1,54 @@
 ﻿using System;
+using System.IO; 
 using System.Net;
-using System.Configuration; 
-
+using System.Text;
+using System.Configuration;
+using System.Security.Cryptography;
 
 namespace KasikLazyMachine
 {
     class Program
-    {       
+    {      
+
         static void Main(string[] args)
         {
+            bool UpToDate = false; 
             string gw2dir = ConfigurationSettings.AppSettings["gw2dir"];
+            bool downloadBuildTemplates = ConfigurationSettings.AppSettings["buildtemplates"].ToUpper() == "Y"; 
             Console.WriteLine("Welcome to the Kasik Lazy Machine, this program will download Arcdps into your\ncomputer."
                 +$" The current download location is at:\n{ gw2dir }\n"+
                 "If you wish to change this please edit the .xml file included with this program."); 
             WebClient webClient = new WebClient();
+            Console.WriteLine("Now connecting to deltaconnected.com and trying to retrieve arcdps...\n");
             try
             {
-                Console.WriteLine("Now connecting to deltaconnected.com and trying to retrieve arcdps...\n"); 
-                webClient.DownloadFile("https://www.deltaconnected.com/arcdps/x64/arcdps.ini", gw2dir + "arcdps.ini");
-                webClient.DownloadFile("https://www.deltaconnected.com/arcdps/x64/d3d9.dll", gw2dir + "d3d9.dll");             
-                Console.WriteLine("\n\nArcdps has been downloaded. Thanks for using the Kasik Lazy Machine.\n" + 
-                    "If you have any questions about this program \nfeel free to message me ingame to Bruno.8937"); 
+                string dllRoute = gw2dir + "d3d9.dll";
+                if (File.Exists(dllRoute))
+                {
+                    string localMd5, remoteMd5;
+                    MD5 md5 = MD5.Create();
+                    using (var stream = File.OpenRead(dllRoute))
+                    {
+                        localMd5 = string.Join(string.Empty, Array.ConvertAll(md5.ComputeHash(stream), b => b.ToString("x2")));
+                    }
+                    remoteMd5 = Encoding.UTF8.GetString(webClient.DownloadData("https://www.deltaconnected.com/arcdps/x64/d3d9.dll.md5sum"));
+                    UpToDate = remoteMd5.Contains(localMd5); 
+                    if (UpToDate) Console.WriteLine("ArcDPS is up to date.");
+                    else Console.WriteLine("Your ArcDPS is not up to date.");
+                }   else Console.WriteLine("You haven't installed ArcDPS yet.");
+
+                if (!UpToDate)
+                {
+                    webClient.DownloadFile("https://www.deltaconnected.com/arcdps/x64/arcdps.ini", gw2dir + "arcdps.ini");
+                    webClient.DownloadFile("https://www.deltaconnected.com/arcdps/x64/d3d9.dll", gw2dir + "d3d9.dll");
+                    if (downloadBuildTemplates)
+                    {
+                        webClient.DownloadFile("https://www.deltaconnected.com/arcdps/x64/buildtemplates/d3d9_arcdps_buildtemplates.dll",
+                            gw2dir + "d3d9_arcdps_buildtemplates.dll"); 
+                    }
+                    Console.WriteLine("\n\nArcdps has been downloaded. Thanks for using the Kasik Lazy Machine.\n" + 
+                        "If you have any questions about this program \nfeel free to message me ingame to Bruno.8937"); 
+                }
             }
             catch (WebException ex)
             {
@@ -36,6 +64,7 @@ namespace KasikLazyMachine
                     "\nIf the following message doesn't give you enough information\nas to what went wrong" + 
                     $"\nfeel free to message me ingame to Bruno.8937\n\n{ex.Message}");
             }
+            
             Console.WriteLine("\nPress Enter to close this window."); 
             Console.ReadLine();
         }
